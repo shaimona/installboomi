097c01d83aeb7de7ed32f7e3ced5f92aad1f7a87
shaimona <shaimon@gmail.com>
2019-07-31 08:13:03 -0700

    Update atom_install.sh

--- atom_install.sh
+++ atom_install.sh
@@ -11,13 +11,13 @@ else
     wget -O /tmp/{{BoomiType}}_install64.sh https://platform.boomi.com/atom/"$lowercaseBoomiType"_install64.sh
     chmod +x /tmp/{{BoomiType}}_install64.sh
     # Install Boomi (Cloud) Molecule
-    /tmp/{{BoomiType}}_install64.sh -q -console -VlocalPath=/usr/local/boomi/work -VinstallToken={{BoomiInstallToken}} -VatomName={{BoomiName}} -dir /mnt/data
+    /tmp/{{BoomiType}}_install64.sh -q -console -VlocalPath=/usr/local/boomi/work -VinstallToken={{BoomiInstallToken}} -VatomName={{BoomiName}} -dir /mnt/data/molecule
     sleep 10
     # Initial setup: UNICAST and JGroups config
-    echo 'com.boomi.container.cloudlet.clusterConfig=UNICAST' >> /mnt/data/{{BoomiType}}_{{BoomiName}}/conf/container.properties
-    echo "com.boomi.container.cloudlet.initialHosts=$(cat /mnt/data/BOOMI_{{BoomiName}}_HEAD_NODE_IP)[7800]" >> /mnt/data/{{BoomiType}}_{{BoomiName}}/conf/container.properties
+    echo 'com.boomi.container.cloudlet.clusterConfig=UNICAST' >> /mnt/data/molecule/{{BoomiType}}_{{BoomiName}}/conf/container.properties
+    echo "com.boomi.container.cloudlet.initialHosts=$(cat /mnt/data/molecule/BOOMI_{{BoomiName}}_HEAD_NODE_IP)[7800]" >> /mnt/data/molecule/{{BoomiType}}_{{BoomiName}}/conf/container.properties
     # Increase JVM heap space
-    sed -i 's/-Xmx.*/-Xmx1024m/g' /mnt/data/{{BoomiType}}_{{BoomiName}}/bin/atom.vmoptions
+    sed -i 's/-Xmx.*/-Xmx1024m/g' /mnt/data/molecule/{{BoomiType}}_{{BoomiName}}/bin/atom.vmoptions
     # Remove installation mutex
     rm /mnt/data/BOOMI_{{BoomiName}}_INPROGRESS
 fi
